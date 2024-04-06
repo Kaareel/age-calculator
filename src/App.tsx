49d25136eb1785dbe6a01cfffa0f826fac3898a0
Manuel Venegas <mengasis@outlook.com>
2024-04-05 22:51:36 -0300

    fix: added validation to calculate age

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,44 @@ import Calculator from "./components/Calculator";
 import Input from "./components/Input";
 import Icon from "./components/Icon";
 
+function calculatorAge(year: string, month: string, day: string) {
+  const yearNum = Number(year);
+  const monthNum = Number(month);
+  const dayNum = Number(day);
+
+  const birthday = new Date(yearNum, monthNum - 1, dayNum);
+  const today = new Date();
+  let age = today.getFullYear() - birthday.getFullYear();
+  let mth = today.getMonth() - birthday.getMonth();
+  let d = today.getDate() - birthday.getDate();
+
+  if (mth < 0 || (mth === 0 && d < 0)) {
+    age--;
+  }
+  if (mth < 0) {
+    mth += 12;
+  }
+  if (d < 0) {
+    let lastDayMonthPrevious = new Date(
+      birthday.getFullYear(),
+      today.getMonth(),
+      0
+    ).getDate();
+    d += lastDayMonthPrevious;
+    mth--;
+  }
+  return {
+    year: `${age}`,
+    month: `${mth}`,
+    day: `${d}`,
+  };
+}
+
 function App() {
   const [day, setDay] = useState("");
   const [month, setMonth] = useState("");
   const [year, setYear] = useState("");
+  const [age, setAge] = useState({year: '--', month: '--', day: '--'})
   const [error, setError] = useState<{ [key: string]: string }>({});
 
   const handleChangeDay = (e: any) => {
@@ -22,44 +56,39 @@ function App() {
     setYear(value);
   };
 
-  function validate() {
-    const newError: { [key: string]: string } = {
-      day: "",
-      month: "",
-      year: "",
-    };
+  function validate(year: string, month: string, day: string) { 
+    const newError: { [key: string]: string } = {}
     if (!day) {
-      console.log(day);
       newError.day = "This field is required";
     } else if (Number(day) >= 32) {
       newError.day = "Must be a valid day";
-    } else {
-      newError.day = "";
-    }
+    } 
+
     if (!month) {
       newError.month = "This field is required";
     } else if (Number(month) >= 13) {
       newError.month = "Must be a valid month";
-    } else {
-      newError.month = "";
     }
+
     if (!year) {
       newError.year = "This field is required";
     } else if (Number(year) >= 2024) {
       newError.year = "Must be in past";
-    } else {
-      newError.year = "";
     }
 
     return newError;
   }
+
   const handleSubmit = (e: any) => {
     e.preventDefault();
-    const errors = validate();
+    const errors = validate(year, month, day);
     setError(errors);
+    console.log(Object.values(errors));
     if (Object.values(errors).length > 0) {
       return;
     }
+    const result = calculatorAge(year, month, day)
+    setAge(result)
   };
 
   return (
@@ -87,10 +116,11 @@ function App() {
             />
             </div>
             <div>
+              {/* biome-ignore lint/a11y/useButtonType: <explanation> */}
               <button className="bg-bgButton rounded-full absolute right-0"><Icon/></button>
             </div>
           </form>
-        <Calculator year={year} month={month} day={day} />
+        <Calculator year={age.year} month={age.month} day={age.day} />
       </div>
     </div>
   );
--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -5,54 +5,18 @@ interface Props {
   day: string;
 }
 
-function calculatorAge(props: Props) {
-  const { year, month, day } = props;
-  const yearNum = Number(year);
-  const monthNum = Number(month);
-  const dayNum = Number(day);
-
-  const birthday = new Date(yearNum, monthNum - 1, dayNum);
-  const today = new Date();
-  console.log("date:", birthday)
-  let age = today.getFullYear() - birthday.getFullYear();
-  let mth = today.getMonth() - birthday.getMonth();
-  let d = today.getDate() - birthday.getDate();
-
-  if (mth < 0 || (mth === 0 && d < 0)) {
-    age--;
-  }
-  if (mth < 0) {
-    mth += 12;
-  }
-  if (d < 0) {
-    let lastDayMonthPrevious = new Date(
-      birthday.getFullYear(),
-      today.getMonth(),
-      0
-    ).getDate();
-    d += lastDayMonthPrevious;
-    mth--;
-  }
-  return {
-    year: age,
-    month: mth,
-    day: d,
-  };
-}
-
 function Calculator(props: Props) {
-    const age = calculatorAge(props)
 
     return (
       <div>
         <p>
-           {age.year} años
+           {props.year} años
         </p>
         <p>
-        {age.month} meses
+        {props.month} meses
         </p>
         <p>
-        {age.day} días
+        {props.day} días
         </p>
       </div>
     );
